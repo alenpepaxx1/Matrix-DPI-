24cda8a6aa1dac4608475b7a4dc7ceaae0dc29b7
Alen Pepa <xalenpepa@gmail.com>
2026-07-01 06:25:52 -0700

    Add utility function for class name merging

---
+++ lib/utils.ts
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2026 Alen Pepa
+ * All rights reserved.
+ */
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
